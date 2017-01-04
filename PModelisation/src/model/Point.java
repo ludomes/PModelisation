3ae7bb856c61fc446bdb9a6489d40c74b0aaca92
Ludovic MESSUVE <ludomes@free.fr>
2017-01-04 17:15:38 +0100

    Model - Suite

--- PModelisation/src/model/Point.java
+++ PModelisation/src/model/Point.java
@@ -1,5 +1,18 @@
 package model;
 
 public class Point {
+	private Double valeur;
+	private String temps;
+	private Integer tempsRelative;
 
+	Point () {
+		
+	}
+	
+	Point (Double valeur, String temps) {
+		this.valeur = valeur;
+		this.temps = temps;
+	}
+	
+	
 }
---
+++ PModelisation/src/model/Value.java
@@ -0,0 +1,5 @@
+package model;
+
+public class Value extends Node {
+
+}
