347a9e03031d5a20096ddcb4056a68e642fd9a7b
Ludovic MESSUVE <ludomes@free.fr>
2017-01-12 14:07:09 +0100

    Ajout de la librairie JFreeChart

--- PModelisation/src/model/Scene.java
+++ PModelisation/src/model/Scene.java
@@ -9,10 +9,9 @@ public abstract class Scene {
 	public abstract Double getValue();
 	public abstract ArrayList<Point> getList();
 	
-	//abstract public Node setValue();
+	//
 	abstract public void setValue(Double value);
 	abstract public void setList(ArrayList<Point> p);
-	////abstract public void addPoint(Point p);
 	
     abstract public boolean estVide();
 
@@ -27,9 +26,6 @@ public abstract class Scene {
     public int nombreEnfants () {
     	return this.enfants.size();
     }
-    
+     
     
 }
-
-//abstract public Node racine();
-//abstract public Node getValue();
