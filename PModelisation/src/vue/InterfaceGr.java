cb998dd2632304f370fc5194563f9869b605574d
Rabah BOUDERKA <phantom@MacBook-Pro-de-Rabah.local>
2017-01-12 08:39:22 +0100

    modif interface gr

--- PModelisation/src/vue/InterfaceGr.java
+++ PModelisation/src/vue/InterfaceGr.java
@@ -2,46 +2,106 @@ package vue;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Vector;
 
 import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeModel;
 
 public class InterfaceGr {
+	
+	private JTable tableau;
 
 	public InterfaceGr() {
-		//Creation de la fenetre
+		
+		// Creation de la FENETRE
 		JFrame fenetre = new JFrame();
 		fenetre.setTitle("Projet de Modélisation");
 		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		fenetre.setPreferredSize(new Dimension(800,600));
-		
-		//Creation du menu
+		fenetre.setPreferredSize(new Dimension(800, 600));
+
+		// Creation du MENU
 		JMenuBar menu = new JMenuBar();
 		JMenu opt1 = new JMenu("Fichier");
 		JMenu opt2 = new JMenu("Aide");
 		menu.add(opt1);
 		menu.add(opt2);
-		
-		//Creation des elements dans le menu
+
+		// Creation des elements dans le MENU
 		JMenuItem opt1cmd1 = new JMenuItem("Ouvrir");
 		JMenuItem opt1cmd2 = new JMenuItem("Enregistrer");
 		JMenuItem opt1cmd3 = new JMenuItem("Quitter");
 		JMenuItem opt2cmd1 = new JMenuItem("A Propos");
+
+		JToolBar laBarreDeFer = new JToolBar();
+		laBarreDeFer.setFloatable(false);
+		laBarreDeFer.add(new JTextField("Selectionnez l'operation voulu"));
+		laBarreDeFer.add(new JButton("Translation"));
+		laBarreDeFer.add(new JButton("Exponentiel"));
+		laBarreDeFer.add(new JButton("Logarithme"));
+		laBarreDeFer.add(new JButton("Generer la courbe"));
 		
 		opt1.add(opt1cmd1);
 		opt1.add(opt1cmd2);
 		opt1.add(opt1cmd3);
 		opt2.add(opt2cmd1);
 		
-		//Ajout de la barre de menu au bon endroit
+		JFileChooser fc = new JFileChooser();
+		
+		//Ajout du LISTENER pour " OUVRIR " 
+		opt1cmd1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser chooser = new JFileChooser();
+				chooser.showOpenDialog(null);
+				System.out.println("Fichier séléctionné : " + chooser.getSelectedFile());
+			}
+		});
+		
+		//Ajout du LISTENER pour " ENREGISTRER "  TO DO!
+				opt1cmd2.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						int val_retour = fc.showSaveDialog(null);
+
+			             if (val_retour == JFileChooser.APPROVE_OPTION) {
+			                File fichier = fc.getSelectedFile();
+			                //afficher le chemin absolu du fichier
+			                try {
+								FileOutputStream ExportExcel = new FileOutputStream(fc.getSelectedFile().getPath()+".csv");
+							} catch (FileNotFoundException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+			                System.out.println("Chemin absolu : "+fichier.getAbsolutePath()+"\n");
+			             } else {
+			                  System.out.println("L'enregistrement est annulée\n");
+			             }
+					}
+				});
+		
+		//Ajout du LISTENER pour " QUITTER " 
+				opt1cmd3.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						System.exit(0);
+					}
+				});
+
+		// Ajout de la BARRE DE MENU au bon endroit
 		fenetre.setJMenuBar(menu);
 		
-			//GROUPE ELEMENT DE GAUCHE
+		//Creation du TREEMODEL
+		//StandardAndPoors model = new StandardAndPoors();
+
+		// GROUPE ELEMENT DE GAUCHE
 		JPanel gauche = new JPanel(new GridLayout(0, 1));
+
 		
-		//Creation du regroupement de boutton
+		// Creation du regroupement de BOUTON
 		ButtonGroup group = new ButtonGroup();
-		
-		//Creation des bouttons independement
+
+		// Creation des BOUTONS independement
 		JRadioButton ch1 = new JRadioButton("Choix 1");
 		ch1.setMnemonic(KeyEvent.VK_1);
 		ch1.setActionCommand("Choix_1");
@@ -50,12 +110,12 @@ public class InterfaceGr {
 		JRadioButton ch2 = new JRadioButton("Choix 2");
 		ch2.setMnemonic(KeyEvent.VK_2);
 		ch2.setActionCommand("Choix_2");
-		
+
 		JRadioButton ch3 = new JRadioButton("Choix 3");
 		ch3.setMnemonic(KeyEvent.VK_3);
 		ch3.setActionCommand("Choix_3");
-		
-		//Ajout des bouttons
+
+		// Ajout des BOUTONS
 		group.add(ch1);
 		gauche.add(ch1);
 		group.add(ch2);
@@ -63,39 +123,71 @@ public class InterfaceGr {
 		group.add(ch3);
 		gauche.add(ch3);
 		
-			//GROUPE ELEMENT DU CENTRE
+		
+		//TEST DE JTREE
+		/*
+		DefaultMutableTreeNode racine1 =
+			       new DefaultMutableTreeNode("c’est la racine") ;
+			JTree monArbre1 = new JTree(racine1) ;
+			*/
+		
+		//REALISATION D'UN ARBRE
+		/*CheckBoxNode accessibilityOptions[] = {
+		        new CheckBoxNode(
+		            "Move system caret with focus/selection changes", false),
+		        new CheckBoxNode("Always expand alt text for images", true) };
+		    CheckBoxNode browsingOptions[] = {
+		        new CheckBoxNode("Notify when downloads complete", true),
+		        new CheckBoxNode("Disable script debugging", true),
+		        new CheckBoxNode("Use AutoComplete", true),
+		        new CheckBoxNode("Browse in a new process", false) };
+		    Vector accessVector = new NamedVector("Accessibility",
+		        browsingOptions);
+		    
+			//Creation de la JTREE
+			JTree arborescence = new JTree(accessVector);
+		    
+		
+		/*DefaultMutableTreeNode racine2 =
+			       new DefaultMutableTreeNode("Pere");
+			for(int i = 0; i < 3; ++i)
+			   racine2.add( new DefaultMutableTreeNode("Fils"+ i));
+			JTree monArbre2 = new JTree(racine2) ;
+			*/
+			//gauche.add(monArbre1);
+			//gauche.add(monArbre2);
+		    //gauche.add(arborescence);
+
+		// GROUPE ELEMENT DU CENTRE
 		JPanel center = new JPanel(new GridBagLayout());
-		
 		center.setLayout(new BorderLayout());
-		
-		Object[][] donnees = {
-                {"Ludovic", "Messuve", Color.red},
-                {"Ludovic", "Messuve", Color.black},
-                {"Ludovic", "Messuve", Color.cyan},
-                {"Ludovic", "Messuve", Color.blue},
-                {"Ludovic", "Messuve", Color.magenta},
-                {"Ludovic", "Messuve", Color.yellow},
-                {"Ludovic", "Messuve", Color.pink},
-        };
-		
-		String[] entetes = {"Abcisses", "Ordonnées", "Resultats"};
-		 
-		JTable tableau = new JTable(donnees, entetes);
-		
-		center.add(tableau.getTableHeader(),BorderLayout.NORTH);
+
+		Object[][] donnees = { { 2, 2 }, { 2, 2},
+				{ 2, 2}, { 2, 2},
+				{ 2, 2}, { 2, 2},
+				{ 2, 2}, };
+
+		String[] entetes = { "Abcisses", "Ordonnées"};
+
+		tableau = new JTable(donnees, entetes);
+
+		center.add(tableau.getTableHeader(), BorderLayout.NORTH);
 		center.add(tableau, BorderLayout.CENTER);
-		 
-        //fenetre.getContentPane().add(tableau.getTableHeader(), BorderLayout.CENTER);
-        //fenetre.getContentPane().add(tableau, BorderLayout.SOUTH);
-		
-		//Ajout des panels dans la fenetre
+
+		JTree arbre = new JTree();
+
+		// Ajout des PANELS dans la fenetre
 		fenetre.getContentPane().add(gauche, BorderLayout.WEST);
 		fenetre.getContentPane().add(center, BorderLayout.CENTER);
-		//fenetre.getContentPane().add(center, BorderLayout.CENTER);
-		
-		//Formatage de la fenetre
+		fenetre.getContentPane().add(laBarreDeFer, BorderLayout.NORTH);
+
+		// Formatage de la FENETRE
 		fenetre.pack();
 		fenetre.setVisible(true);
+	}
+	
+	public JTable getTable(){
+		return tableau;
 	}
 
 	public static void main(String[] args) {
