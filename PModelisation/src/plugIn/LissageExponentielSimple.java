5b6e611a8938cc9fde0fdb807701506ffd5792b8
Florantin Bocquet <paradoxiforme@gmail.com>
2017-01-18 13:52:34 +0100

    maj

--- PModelisation/src/plugIn/LissageExponentielSimple.java
+++ PModelisation/src/plugIn/LissageExponentielSimple.java
@@ -2,15 +2,47 @@ package plugIn;
 
 import javax.swing.JPanel;
 
+import model.Point;
 import model.Scene;
+import model.Serie;
 
 public class LissageExponentielSimple extends Module
 {
-	
 	@Override
 	public Scene transformer()
 	{
-		return null;
+		double b = (double) contexte.getParametre("Beta");
+		int t = (int) contexte.getParametre("tour");
+		
+		Scene s = (Scene) contexte.getParametre("Scene");
+		if(s == null)	return s;
+		if(s.getList() == null)	return s;
+		
+		//duplication s
+		Serie serie = new Serie();
+		
+		int T = serie.getList().size();
+		for(int i = 0; i < t; i ++)
+		{
+			double value = 0;
+			
+			for(int j = 0; j < T + i - 1; j++)
+			{
+				//value += (b**j) * serie.getList().get(T + i - j).getValeur();
+			}
+			
+			double prediction = (1 - b) * value;
+			
+			String date = serie.getList().get(T + i - 1).getDate(); //intervealle entre chaque valeur
+			
+			Point p = new Point(prediction, date);
+			p.setTempsRelative(0);
+			
+			serie.getList().add(p);
+		}
+	
+		
+		return serie;
 	}
 	
 	@Override
--- PModelisation/src/plugIn/Module.java
+++ PModelisation/src/plugIn/Module.java
@@ -1,6 +1,11 @@
 package plugIn;
 
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import model.Scene;
 
@@ -15,5 +20,32 @@ public abstract class Module
 	public final String getName()
 	{
 		return this.getName();
+	}
+	
+	
+	protected JPanel setPanel(String[] parametres)
+	{
+		JPanel panel = new JPanel();
+		
+		GridLayout lay = new GridLayout(2, parametres.length + 1);
+		
+		for(int i = 0; i < parametres.length - 1; i++)
+		{
+			lay.addLayoutComponent(parametres[i], new JLabel(parametres[i]));
+			lay.addLayoutComponent(parametres[i], new JTextField(parametres[i]));
+		}
+		
+		JButton valider = new JButton("Valider");
+		//Ajout Action Listener;
+		
+		JButton annuler = new JButton("Annuler");
+		//Ajout Action Listener;
+		
+		lay.addLayoutComponent("Valider", valider);
+		lay.addLayoutComponent("Annuler", annuler);
+		
+		panel.setLayout(lay);
+		
+		return panel;
 	}
 }
